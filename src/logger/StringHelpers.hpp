8a97f81d2af93bb56897390638359cac7016dd5e
jjasinski <ja.jasinski@gmail.com>
2016-10-03 23:25:33 +0200

    refactoring

--- src/logger/StringHelpers.hpp
+++ src/logger/StringHelpers.hpp
@@ -11,10 +11,10 @@ namespace logger
   {
     size_t size = snprintf(nullptr, 0, format.c_str(), args ...) + 1; // extra place for '\0'
 
-    std::string result(size, '\0');
-    //result.resize(size);
+    std::string result;
+    result.resize(size);  // reserve space for the last '\0' character also
     snprintf(&result[0], size, format.c_str(), args ...); // generate string with '\0' at the end
-    result.resize(size - 1); // cut the last '\0' character
+    result.pop_back();    // cut the last '\0' character
     return std::move(result);
   }
 
